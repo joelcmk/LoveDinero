dbd6334b9b4d2726e07a366b44942c8fcbd50a51
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-11-15 14:26:48 -0600

    added expence and category functions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+const App = function () {
+
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState('')
+  const [submit, setSubmit] = useState('')
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItems(item)
+    setSubmit('next')
+  }
+
+  if (submit === '') {
+    return (
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <label>Add expense</label>
+          <input name="expense" id="expense" value={item} onChange={handleChange} />
+          <button type="submit">Next</button>
+        </form>
+      </div>
+    );
+  }
+
+  if (submit === 'next') {
+    return (
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <label>Select a category</label>
+          <input name="expense" id="expense" value={item} onChange={handleChange} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+};
 
 export default App;
