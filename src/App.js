65fa442e71b0b0a0bbadcc83bf6bb6b9ce0abd61
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-11-16 18:48:06 -0600

    Added input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
+import Select from 'react-select';
+import Creatable, { useCreatable } from 'react-select/creatable';
+import CreatableSelect from 'react-select/creatable';
 import './App.css';
 
 const App = function () {
 
   const [item, setItem] = useState('');
   const [items, setItems] = useState('')
-  const [submit, setSubmit] = useState('')
+  const [submit, setSubmit] = useState('');
+  const [test, setTest] = useState('')
+  const [test2, setTest2] = useState('');
+
+  const options = [
+    { value: 'home', label: 'Home' },
+    { value: 'food', label: 'Food' },
+    { value: 'shopping', label: 'Shopping' },
+    { value: 'utillities', label: 'Utillities' },
+    { value: 'household', label: 'Household' },
+    { value: 'transportation', label: 'Transportation' },
+    { value: 'other', label: 'Other' }
+  ]
 
   const handleChange = (e) => {
     setItem(e.target.value);
@@ -16,12 +31,23 @@ const App = function () {
     setItems(item)
     setSubmit('next')
   }
+  const hello = (e) => {
+    e.preventDefault()
+    setTest2(test)
+    setSubmit('')
+  }
+
+  const test1 = (e) => {
+    setTest(e.value)
+  };
+
+  console.log(test2)
 
   if (submit === '') {
     return (
       <div className="App">
         <form onSubmit={handleSubmit}>
-          <label>Add expense</label>
+          <label>Add New Expense</label>
           <input name="expense" id="expense" value={item} onChange={handleChange} />
           <button type="submit">Next</button>
         </form>
@@ -31,13 +57,14 @@ const App = function () {
 
   if (submit === 'next') {
     return (
-      <div className="App">
-        <form onSubmit={handleSubmit}>
-          <label>Select a category</label>
-          <input name="expense" id="expense" value={item} onChange={handleChange} />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+      <form onSubmit={hello} className="App">
+        <CreatableSelect
+          options={options}
+          isClearable
+          onChange={test1}
+        />
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 };
