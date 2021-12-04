ea49701fc864a2c8984601ecbb42206add7591be
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-12-04 15:57:48 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,23 +5,21 @@ function Budget(props) {
 
   const [homeTarget, setdomeTarget] = useState('600');
   const [foodTarget, setFoodTarget] = useState('400')
-  const [home, setdome] = useState(40);
+  const [home, setHome] = useState({ edit: true, value: '45' });
   const [food, setFood] = useState('');
   const [shopping, setShopping] = useState('');
   const [utilities, setUtilities] = useState('');
   const [household, setHousehold] = useState('');
-  const [transportation, setTransportation] = useState(`$${40}`);
+  const [transportation, setTransportation] = useState();
 
-  const handleSubmit = (e, target, val) => {
+  const [isNotEditing, setIsNotEditing] = useState(true);
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    target(home);
-    setIsNotEditing(false);
+    setHome({ edit: true })
   }
 
-  const [isNotEditing, setIsNotEditing] = useState(true)
-  const [test, setTest] = useState(true)
-
-
+  console.log(home.value)
 
   return (
     <div className="budget">
@@ -34,82 +32,82 @@ function Budget(props) {
           </tr>
           <tr>
             <td>Home</td>
-            <td>69</td>
+            <td>{props.homeTotal}</td>
             <td>{
-              test ?
+              home.edit ?
                 <div className="edit">
-                  <p className="test1">{home}</p>
-                  <button onClick={() => setIsNotEditing(false)} type="submit">edit</button>
+                  <p className="test1">{home.value}</p>
+                  <button onClick={() => setHome({ edit: false })} type="submit">edit</button>
                 </div>
-                : <form>
-                  <input onChange={(e) => setTransportation(e.target.value)} />
-                  <button onClick={() => setIsNotEditing(true)}>Submit</button>
-                </form>
+                : <div>
+                  <input onChange={(e) => setHome({ value: 33 })} />
+                  <button onClick={() => setHome({ edit: true })}>Submit</button>
+                </div>
             }</td>
           </tr>
           <tr>
             <td>Food</td>
-            <td>69</td>
+            <td>{props.foodTotal}</td>
             <td className="test2">{
-              test ?
+              isNotEditing ?
                 <div className="edit">
-                  <p className="test1">{transportation}</p>
+                  <p className="test1">{food}</p>
                   <button onClick={() => setIsNotEditing(false)} type="submit">edit</button>
                 </div>
                 : <form>
-                  <input onChange={(e) => setTransportation(e.target.value)} />
+                  <input onChange={(e) => setFood(`$${e.target.value}`)} />
                   <button onClick={() => setIsNotEditing(true)}>Submit</button>
                 </form>
             }</td>
           </tr>
           <tr>
             <td>Shopping</td>
-            <td>69</td>
+            <td>{props.shoppingTotal}</td>
             <td>{
-              test ?
+              isNotEditing ?
                 <div className="edit">
-                  <p className="test1">{transportation}</p>
+                  <p className="test1">{shopping}</p>
                   <button onClick={() => setIsNotEditing(false)} type="submit">edit</button>
                 </div>
                 : <form>
-                  <input onChange={(e) => setTransportation(e.target.value)} />
+                  <input onChange={(e) => setShopping(`$${e.target.value}`)} />
                   <button onClick={() => setIsNotEditing(true)}>Submit</button>
                 </form>
             }</td>
           </tr>
           <tr>
             <td>Utilities</td>
-            <td>69</td>
+            <td>{props.utilitiesTotal}</td>
             <td>{
-              test ?
+              isNotEditing ?
                 <div className="edit">
-                  <p className="test1">{transportation}</p>
+                  <p className="test1">{utilities}</p>
                   <button onClick={() => setIsNotEditing(false)} type="submit">edit</button>
                 </div>
                 : <form>
-                  <input onChange={(e) => setTransportation(e.target.value)} />
+                  <input onChange={(e) => setUtilities(`$${e.target.value}`)} />
                   <button onClick={() => setIsNotEditing(true)}>Submit</button>
                 </form>
             }</td>
           </tr>
           <tr>
             <td>Household</td>
-            <td>69</td>
+            <td>{props.householdTotal}</td>
             <td>{
-              test ?
+              isNotEditing ?
                 <div className="edit">
-                  <p className="test1">{transportation}</p>
+                  <p className="test1">{household}</p>
                   <button onClick={() => setIsNotEditing(false)} type="submit">edit</button>
                 </div>
                 : <form>
-                  <input onChange={(e) => setTransportation(e.target.value)} />
+                  <input onChange={(e) => setHousehold(`$${e.target.value}`)} />
                   <button onClick={() => setIsNotEditing(true)}>Submit</button>
                 </form>
             }</td>
           </tr>
           <tr>
             <td>Transportation</td>
-            <td>69</td>
+            <td>{props.transportationTotal}</td>
             <td>{
               isNotEditing ?
                 <div className="edit">
@@ -124,7 +122,7 @@ function Budget(props) {
           </tr>
         </table>
       </div>
-    </div>
+    </div >
   );
 }
 
