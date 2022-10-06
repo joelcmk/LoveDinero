945eefb3ed1440ebe3fae8bbdf2cdca1cf1afc88
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-10-05 22:14:58 -0500

    Fix update button

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,24 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import Input from '../../components/Input';
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate, Link } from 'react-router-dom';
 import firebase from 'firebase/compat/app';
 import './Budget.css';
 import Navbar from '../Navbar';
 
-import { getDatabase, ref, onValue, update, set, child } from "firebase/database";
-
+import {
+  getDatabase,
+  ref,
+  onValue,
+  update,
+  set,
+  child,
+} from 'firebase/database';
 
 function Budget(props) {
-
-  const [home, setHome] = useState('300');
-  const [food, setFood] = useState('300');
-  const [shopping, setShopping] = useState('300');
-  const [utilities, setUtilities] = useState('300');
-  const [household, setHousehold] = useState('300');
-  const [transportation, setTransportation] = useState('300');
-  const [other, setOther] = useState('300');
-
   const [homeEdit, setHomeEdit] = useState(true);
   const [foodEdit, setFoodEdit] = useState(true);
   const [shoppingEdit, setShoppingEdit] = useState(true);
@@ -28,30 +25,26 @@ function Budget(props) {
   const [otherEdit, setOtherEdit] = useState(true);
 
   const [userId, setUserId] = useState();
-  const [target, setTarget] = useState('')
-  const [data, setData] = useState('');
-
-
-
-
-  const auth = getAuth();
-  const user = auth.currentUser;
-
-
-  const navigate = useNavigate();
 
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        setUserId(uid)
+    const db = getDatabase();
+    const starCountRef = ref(db, 'users/' + userId + '/target');
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data == null) {
+        setTarget({ home: 0 });
       } else {
-        navigate('/login')
+        setTarget(data);
+        setHome(data.home);
+        setFood(data.food);
+        setShopping(data.shopping);
+        setUtilities(data.utilities);
+        setHousehold(data.household);
+        setTransportation(data.transportation);
+        setOther(data.other);
       }
     });
-  });
-
-
+  }, [userId]);
 
   useEffect(() => {
     const db = getDatabase();
@@ -60,24 +53,65 @@ function Budget(props) {
       const data = snapshot.val();
       const list = [];
       for (let id in data) {
-        list.push(data)
+        list.push(data);
       }
       //setTarget(data)
     });
-  }, [userId]);
+  }, []);
+  const [target, setTarget] = useState('');
+
+  const [home, setHome] = useState('0');
+  const [food, setFood] = useState('0');
+  const [shopping, setShopping] = useState('0');
+  const [utilities, setUtilities] = useState('0');
+  const [household, setHousehold] = useState('0');
+  const [transportation, setTransportation] = useState('0');
+  const [other, setOther] = useState('0');
+
+  const [data, setData] = useState('');
+
+  const [updateTarget, setUpdateTarget] = useState(false);
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const navigate = useNavigate();
+
+  console.log(transportation);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        setUserId(uid);
+      } else {
+        navigate('/login');
+      }
+    });
+  });
 
   function submitHome() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      home: home
+      home: home,
     });
-    setHomeEdit(true);
+    setUpdateTarget(!updateTarget);
+  }
+
+  function allCategoriesTarget() {
+    setUpdateTarget(!updateTarget);
+    const db = getDatabase();
+    if (updateTarget) {
+      update(ref(db, 'users/' + userId + '/target'), {
+        home: home,
+      });
+    }
   }
 
   function submitFood() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      food: food
+      food: food,
     });
     setFoodEdit(true);
   }
@@ -85,7 +119,7 @@ function Budget(props) {
   function submitShopping() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      shopping: shopping
+      shopping: shopping,
     });
     setShoppingEdit(true);
   }
@@ -93,7 +127,7 @@ function Budget(props) {
   function submitUtilities() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      utilities: utilities
+      utilities: utilities,
     });
     setUtilitiesEdit(true);
   }
@@ -101,7 +135,7 @@ function Budget(props) {
   function submitHousehold() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      household: household
+      household: household,
     });
     setHouseholdEdit(true);
   }
@@ -109,7 +143,7 @@ function Budget(props) {
   function submitTransportation() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      transportation: transportation
+      transportation: transportation,
     });
     setTransportationEdit(true);
   }
@@ -117,35 +151,70 @@ function Budget(props) {
   function submitOther() {
     const db = getDatabase();
     update(ref(db, 'users/' + userId + '/target'), {
-      other: other
+      other: other,
     });
     setOtherEdit(true);
   }
 
-
-
-  useEffect(() => {
-    const db = getDatabase();
-    const starCountRef = ref(db, 'users/' + userId + '/target');
-    onValue(starCountRef, (snapshot) => {
-      const data = snapshot.val();
-      if (data == null) {
-        setTarget({ home: 0 })
-      } else {
-        setTarget(data)
-      }
-    });
-  }, [userId]);
-
-  console.log(target)
-
+  const allData = [
+    {
+      categoryName: 'Home',
+      category: home,
+      expenses: props.categoryTotal('home'),
+      target: home,
+      updateTarget: setHome,
+    },
+    {
+      categoryName: 'food',
+      category: food,
+      expenses: props.categoryTotal('food'),
+      target: target.food,
+      updateTarget: setFood,
+    },
+    {
+      categoryName: 'Shopping',
+      category: shopping,
+      expenses: props.categoryTotal('shopping'),
+      target: target.shopping,
+      updateTarget: setShopping,
+    },
+    {
+      categoryName: 'Utilities',
+      category: utilities,
+      expenses: props.categoryTotal('utilities'),
+      target: target.utilities,
+    },
+    {
+      categoryName: 'Household',
+      category: household,
+      expenses: props.categoryTotal('household'),
+      target: target.household,
+    },
+    {
+      categoryName: 'Transportation',
+      category: transportation,
+      expenses: props.categoryTotal('transportation'),
+      target: target.transportation,
+    },
+    {
+      categoryName: 'Other',
+      category: other,
+      expenses: props.categoryTotal('other'),
+      target: target.other,
+      updateTarget: setOther,
+    },
+  ];
 
   if (target) {
-    console.log(target.shopping)
     return (
       <div>
         <Navbar />
-        <Input setExpense={props.setExpense} setSubmit={props.setSubmit} income={props.income} expense={props.expense} />
+        <Input
+          setExpense={props.setExpense}
+          setSubmit={props.setSubmit}
+          income={props.income}
+          expense={props.expense}
+        />
 
         <div className="budget">
           <div className="test">
@@ -155,127 +224,57 @@ function Budget(props) {
                 <th>Expenses</th>
                 <th>Target</th>
               </tr>
-              <tr>
-                <td>Home</td>
-                <td><span>$</span>{props.categoryTotal('home')}</td>
-                <td>{
-                  homeEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.home == undefined ? '0' : target.home}</p>
-                      <button onClick={() => setHomeEdit(false)} type="submit">edit</button>
-                    </div>
-                    : <div>
-                      <input type="number" onChange={(e) => setHome(e.target.value)} />
-                      <button onClick={() => submitHome()}>Submit</button>
-                    </div>
-                }</td>
-              </tr>
-              <tr>
-                <td>Food</td>
-                <td><span>$</span>{props.categoryTotal('food')}</td>
-                <td className="test2">{
-                  foodEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.food == undefined ? '0' : target.food}</p>
-                      
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setFood(e.target.value)} />
-                      <button onClick={() => submitFood()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
-              <tr>
-                <td>Shopping</td>
-                <td><span>$</span>{props.categoryTotal('shopping')}</td>
-                <td>{
-                  shoppingEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.shopping == undefined ? '0' : target.shopping}</p>
-                      
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setShopping(e.target.value)} />
-                      <button onClick={() => submitShopping()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
-              <tr>
-                <td>Utilities</td>
-                <td><span>$</span>{props.categoryTotal('utilities')}</td>
-                <td>{
-                  utilitiesEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.utilities == undefined ? '0' : target.utilities}</p>
-                      
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setUtilities(e.target.value)} />
-                      <button onClick={() => submitUtilities()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
-              <tr>
-                <td>Household</td>
-                <td><span>$</span>{props.categoryTotal('household')}</td>
-                <td>{
-                  householdEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.household == undefined ? '0' : target.household}</p>
-                      
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setHousehold(e.target.value)} />
-                      <button onClick={() => submitHousehold()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
-              <tr>
-                <td>Transportation</td>
-                <td><span>$</span>{props.categoryTotal('transportation')}</td>
-                <td>{
-                  transportationEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.transportation == undefined ? '0' : target.transportation}</p>
-                     
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setTransportation(e.target.value)} />
-                      <button onClick={() => submitTransportation()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
-              <tr>
-                <td>Other</td>
-                <td><span>$</span>{props.categoryTotal('other')}</td>
-                <td>{
-                  otherEdit ?
-                    <div className="edit">
-                      <p className="test1"><span>$</span>{target.other == undefined ? '0' : target.other}</p>
-                      
-                    </div>
-                    : <form>
-                      <input type="number" onChange={(e) => setOther(e.target.value)} />
-                      <button onClick={() => submitOther()}>Submit</button>
-                    </form>
-                }</td>
-              </tr>
+              {allData.map((item) => (
+                <>
+                  <tr>
+                    <td>{item.categoryName}</td>
+                    <td>
+                      <span>$</span>
+                      {item.expenses}
+                    </td>
+                    <td>
+                      {!updateTarget ? (
+                        <div className="edit">
+                          <p className="test1">
+                            <span>$</span>
+                            {item.target == undefined ? '0' : item.target}
+                          </p>
+                        </div>
+                      ) : (
+                        <div>
+                          <input
+                            type="number"
+                            onChange={(e) => item.updateTarget(e.target.value)}
+                            value={item.category ? item.category : '0'}
+                          />
+                        </div>
+                      )}
+                    </td>
+                  </tr>
+                </>
+              ))}
               <tr>
                 <td></td>
                 <td></td>
-                <td><button className="update_button">update</button></td>
+                <td>
+                  <button
+                    className="update_button"
+                    onClick={() => allCategoriesTarget()}
+                  >
+                    {!updateTarget ? 'update' : 'done'}
+                  </button>
+                </td>
               </tr>
             </table>
           </div>
         </div>
-        <Link className="expenses_list" to="/expenses"><button>Expenses List</button></Link>
-      </div >
+        <Link className="expenses_list" to="/expenses">
+          <button>Expenses List</button>
+        </Link>
+      </div>
     );
-  }
-  else {
-    return (
-      <p>loading{console.log(target)}</p>
-    )
+  } else {
+    return <p>loading</p>;
   }
 }
 
